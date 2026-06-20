705cb3897189cd388fd6375ebb0ba86febb78e24
manoj <manojmandala07@gmail.com>
2026-06-20 11:24:59 +0530

    added new feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Home from './Components/Home'
+import About from './Components/About'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+<>
 
   <h1> hello world !</h1>
+  {/* <Home/>
+  <About/> */}
+     {3 < 2 ? <Home/> : <About/>}
+</>
       
   )
 }
---
+++ src/Components/About.jsx
@@ -0,0 +1,15 @@
+export default function About() {
+
+    let name = "About"
+    return (
+        <>
+        <h1>Hello this is {name} job</h1>
+        
+
+
+
+        </>
+
+       
+    )
+}
